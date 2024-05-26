e546f00e480a1736ced154270e1d23a0d867f5ba
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-05-26 21:47:18 +0300

    Initial Commit

---
+++ AwardAnimation/AwardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardAnimationApp.swift
+//  AwardAnimation
+//
+//  Created by Анна on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AwardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
